9e5834a8781930b8c49ad514c34d7e7dca970f41
andy <tmgthemultigamer@gmail.com>
2021-01-05 11:45:04 -0800

    Added smooth camera + camera bounds

--- stevgame/Assets/scripts/Player/CameraController.cs
+++ stevgame/Assets/scripts/Player/CameraController.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // TopLeftBorder and BotRightBorder are two empty game objects that you 
+    // can move around the map to easily change the bounds of the camera.
+    public GameObject TopLeftBorder;
+    public GameObject BotRightBorder;
+    public Camera Cam;
+    public Transform Player;
+    
+    [SerializeField] private float LerpValue;
+    private float _viewWidth, _viewHeight, _leftBound, _rightBound, _topBound, _botBound;
 
-    public Transform Target;
-
-    // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        _viewWidth = 2f * Cam.orthographicSize * Screen.width / Screen.height;
+        _viewHeight = 2f * Cam.orthographicSize * Screen.height / Screen.width;
+        _leftBound = TopLeftBorder.transform.position.x + _viewWidth/2f;
+        _rightBound = BotRightBorder.transform.position.x - _viewWidth/2f;
+        _topBound = TopLeftBorder.transform.position.y - _viewHeight/1.5f;
+        _botBound = BotRightBorder.transform.position.y + _viewHeight/1.5f;
+
+        // Instantiate Camera Position.
+        transform.position = new Vector3(Player.transform.position.x,
+        Player.transform.position.y,-10f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(Target.transform.position.x,
-        Target.transform.position.y,transform.position.z);
+        transform.position = Vector3.Lerp(transform.position,
+        new Vector3(FindXBound(), FindYBound(), transform.position.z), LerpValue);
+    }
+
+    private float FindXBound()
+    {
+        if(Player.transform.position.x < _leftBound) return _leftBound;
+        else if(Player.transform.position.x > _rightBound) return _rightBound;
+        else return Player.transform.position.x;
+    }
+
+    private float FindYBound()
+    {
+        if(Player.transform.position.y > _topBound) return _topBound;
+        else if (Player.transform.position.y < _botBound) return _botBound;
+        else return Player.transform.position.y;
     }
 }
